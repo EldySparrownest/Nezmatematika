5e3fef6b0c41061eaab7c0f2969abcfb114e030c
Zdeňka Kolářová <nezmatematika@gmail.com>
2022-02-05 21:52:34 +0100

    Přejmenování solution

--- MVVMMathProblemsBase/Properties/Resources.Designer.cs
+++ MVVMMathProblemsBase/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MVVMMathProblemsBase.Properties {
+namespace Nezmatematika.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MVVMMathProblemsBase.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MVVMMathProblemsBase.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Nezmatematika.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- MVVMMathProblemsBase/Properties/Settings.Designer.cs
+++ MVVMMathProblemsBase/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MVVMMathProblemsBase.Properties {
+namespace Nezmatematika.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
